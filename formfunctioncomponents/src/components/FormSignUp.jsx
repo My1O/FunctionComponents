2cc15b385b5092927aad5426e2ddc3622aa735b5
Camilo Castro <castroh24@gmail.com>
2023-06-11 21:25:09 -0600

    validar compo nombre: completed

--- formfunctioncomponents/src/components/FormSignUp.jsx
+++ formfunctioncomponents/src/components/FormSignUp.jsx
@@ -5,82 +5,94 @@ import FormGroup from '@mui/material/FormGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import { useState } from 'react'
 
-const FormSignUp = ({handleSubmit}) => {
-    
-    
+const FormSignUp = ({ handleSubmit }) => {
+
+
     const [name, setName] = useState("")
 
     const [lastName, setLastName] = useState("")
     const [email, setEmail] = useState('')
     const [prom, setProm] = useState(true)
-    const [nove,  setNov] = useState(false)
+    const [nove, setNov] = useState(false)
 
     const [errors, setErrors] = useState({
-        name:{
-        error: true, 
-        message: "deben ser 3 caracteres",
-    },
+        name: {
+            error: false,
+            message: "deben ser 3 caracteres",
+        },
     })
 
-    return <form onSubmit={ (e) => {
+    const validarNombre = (nombre) => {
+        if(nombre.length >= 3){
+            return { name: {error: false, message: ''}}
+        } else{
+            return { name: {error: true, message: 'Ingrese al menos 3 caracteres'}}
+        }
+    }
+
+    return <form onSubmit={(e) => {
         //prevent default behavior, post or get
         e.preventDefault()
-        handleSubmit({name, lastName, email, prom, nove})
+        handleSubmit({ name, lastName, email, prom, nove })
     }}>
 
         <TextField
-         id='name' 
-         label="Nombre" 
-         variant='outlined' 
-         fullWidth 
-         margin='normal' 
-         onChange={ (e)=>{
-            console.log(e.target.value)
+            id='name'
+            label="Nombre"
+            variant='outlined'
+            fullWidth
+            margin='normal'
+            onChange={(e) => {
+                console.log(e.target.value)
                 setName(e.target.value)
-         }}
-         value={name}
-         error= {errors.name.error}
-         //use object erros: name with error and message value
-         helperText={errors.name.error ? errors.name.message : ""}
-         />
-        <TextField 
-         id='lastName'
-         label="Apellidos" 
-         variant='outlined' 
-         fullWidth margin='normal' 
-         value={lastName} 
-         onChange={(e) => setLastName(e.target.value)} />
-        <TextField 
-        id='email' 
-        label="Email" 
-        variant='outlined' 
-        fullWidth 
-        margin='normal'
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+            }}
+            value={name}
+            error={errors.name.error}
+            //use object erros: name with error and message value
+            helperText={errors.name.error ? errors.name.message : ""}
+            onBlur={ (e) => {
+                console.log(e)
+                setErrors(validarNombre(e.target.value))
+            }}
+        />
+        <TextField
+            id='lastName'
+            label="Apellidos"
+            variant='outlined'
+            fullWidth margin='normal'
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)} />
+        <TextField
+            id='email'
+            label="Email"
+            variant='outlined'
+            fullWidth
+            margin='normal'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
         />
 
 
         <FormGroup>
             <FormControlLabel control={
-            <Switch 
-            
-            checked={prom} 
-            onChange={(e) => setProm(e.target.checked) }
-            
-            />} label="Promociones"
-             />
-            
-            <FormControlLabel 
-            control={ 
-            <Switch 
-           
-            checked={nove}
-            onChange={ (e) => {
-                setNov(e.target.checked)
-            }}
-            />} 
-            label= "Novedades" 
+                <Switch
+
+                    checked={prom}
+                    onChange={(e) => setProm(e.target.checked)}
+
+                />} label="Promociones"
+            />
+
+            <FormControlLabel
+                control={
+                    <Switch
+
+                        checked={nove}
+                        onChange={(e) => {
+                            setNov(e.target.checked)
+                        }}
+                    />}
+                label="Novedades"
             />
         </FormGroup>
         <Button variant='contained' type='submit'>Registrarse</Button>
